dfd4a8db6ad61e3a8879f17dd255421e6daa2642
Creasure <yurxo@bk.ru>
2022-08-09 18:23:26 +0300

    changed C style to gnu and added last printed character check

--- src/lib.h
+++ src/lib.h
@@ -1,107 +1,134 @@
-typedef struct ret_t { char *cmd, *arr; } ret_t;
+static char lastput = 0;
 
-void rdcmds(char **to, const char *filename)
+struct ret_t
 {
-    char buffer;
-    FILE *input;
-    size_t i = 0, cmdsiz = 0;
-    size_t bopen = 0, bclose = 0;
+  char *cmd, *arr;
+};
 
-    if (!(input = fopen(filename, "r"))) {
-        fprintf(stderr, "No such file: %s\n", filename);
-        exit(1);
+void
+rdcmds (char **to, const char *filename)
+{
+  char buffer;
+  FILE *input;
+  size_t i = 0, cmdsiz = 0;
+  size_t bopen = 0, bclose = 0;
+
+  if (!(input = fopen (filename, "r")))
+    {
+      fprintf (stderr, "No such file: %s\n", filename);
+      exit (1);
     }
 
-    while (fread(&buffer, sizeof(char), 1, input))
-        switch (buffer) {
-            case '[':
-                ++bopen;
-                --bclose;
-            case ']':
-                ++bclose;
-            case '>':
-            case '<':
-            case '+':
-            case '-':
-            case '.':
-            case ',':
-                ++cmdsiz;
+  while (fread (&buffer, sizeof (char), 1, input))
+    switch (buffer)
+      {
+      case '[':
+        ++bopen;
+        --bclose;
+      case ']':
+        ++bclose;
+      case '>':
+      case '<':
+      case '+':
+      case '-':
+      case '.':
+      case ',':
+        ++cmdsiz;
+      }
+  fclose (input);
+
+  if (bopen != bclose)
+    {
+      fprintf (stderr, "Syntax error! the amount of open brackets (%zu)\n"
+               "is not the same as the amount of closing brackets (%zu)\n",
+               bopen, bclose);
+      exit (2);
+    }
+
+  input = fopen (filename, "r");
+  free (*to);
+  *to = (char *) calloc (cmdsiz + 1, sizeof (char));
+
+  while (fread (&buffer, sizeof (char), 1, input))
+    switch (buffer)
+      case '>':
+      case '<':
+      case '+':
+      case '-':
+      case '.':
+      case ',':
+      case '[':
+      case ']':
+      (*to)[i++] = buffer;
+}
+
+struct ret_t
+skip (char *cmd, char *arr)
+{
+  struct ret_t tmp;
+  size_t obmnt = 0;
+
+  do
+    {
+      while (*cmd++ != ']')
+        if (*cmd == '[')
+          ++obmnt;
+    }
+  while (obmnt--);
+
+  tmp.cmd = --cmd;
+  tmp.arr = arr;
+  return tmp;
+}
+
+struct ret_t
+go (char *cmd, char *arr)
+{
+  struct ret_t tmp;
+  while (*cmd != '\0' && *cmd != ']')
+    {
+      switch (*cmd)
+        {
+        case '>':
+          ++arr;
+          break;
+        case '<':
+          --arr;
+          break;
+        case '+':
+          ++*arr;
+          break;
+        case '-':
+          --*arr;
+          break;
+        case '.':
+          lastput = *arr;
+          putchar (lastput);
+          fflush (stdout);
+          break;
+        case ',':
+          *arr = getchar ();
+          break;
+        case '[':
+          if (*arr)
+            {
+              while (*arr)
+                {
+                  tmp = go (cmd + 1, arr);
+                  arr = tmp.arr;
+                }
+              cmd = tmp.cmd;
+            }
+          else
+            {
+              tmp = skip (cmd, arr);
+              cmd = tmp.cmd;
+              arr = tmp.arr;
+            }
         }
-
-    if (bopen != bclose) {
-        fprintf(stderr, "Syntax error! the amount of open brackets (%zu)\n"
-                "is not the same as the amount of closing brackets (%zu)\n",
-                bopen, bclose);
-        exit(2);
+      ++cmd;
     }
-
-    fclose(input);
-    input = fopen(filename, "r");
-
-    free(*to);
-    *to = (char *) calloc(cmdsiz + 1, sizeof(char));
-
-    while (fread(&buffer, sizeof(char), 1, input))
-        switch (buffer)
-            case '>':
-            case '<':
-            case '+':
-            case '-':
-            case '.':
-            case ',':
-            case '[':
-            case ']':
-                (*to)[i++] = buffer;
-}
-
-ret_t skip(char *cmd, char *arr)
-{
-    ret_t tmp;
-    size_t obmnt = 0;
-
-    do {
-        while (*cmd++ != ']')
-            if (*cmd == '[')
-                ++obmnt;
-    } while (obmnt--);
-
-    tmp.cmd = --cmd;
-    tmp.arr = arr;
-    return tmp;
-}
-
-ret_t go(char *cmd, char *arr)
-{
-    ret_t tmp;
-    while (*cmd != '\0' && *cmd != ']') {
-        switch (*cmd) {
-            case '>': ++arr;
-                      break;
-            case '<': --arr;
-                      break;
-            case '+': ++*arr;
-                      break;
-            case '-': --*arr;
-                      break;
-            case '.': putchar(*arr);
-                      fflush(stdout);
-                      break;
-            case ',': *arr = getchar();
-                      break;
-            case '[': if (*arr) {
-                          while (*arr) {
-                              tmp = go(cmd + 1, arr);
-                              arr = tmp.arr;
-                          }
-                          cmd = tmp.cmd;
-                      } else {
-                          tmp = skip(cmd, arr);
-                          cmd = tmp.cmd;
-                          arr = tmp.arr;
-                      }
-        } ++cmd;
-    }
-    tmp.cmd = cmd;
-    tmp.arr = arr;
-    return tmp;
+  tmp.cmd = cmd;
+  tmp.arr = arr;
+  return tmp;
 }
--- src/main.c
+++ src/main.c
@@ -4,31 +4,36 @@
 #define ARRCAP 30000
 #define RELEASE
 
-int main(int argc, char **argv)
+int
+main (int argc, char **argv)
 {
-    char *filename, *cmd = NULL;
-    char arr[ARRCAP] = {};
+  char *filename, *cmd = NULL;
+  char arr[ARRCAP] = { };
 
-    if (argc < 2) {
-        fputs("Not enough arguments!\n"
-              "Please provide the file name!\n",
-              stderr);
-        exit(1);
-    } else {
-        filename = argv[1];
+  if (argc < 2)
+    {
+      fputs ("Not enough arguments!\n"
+             "Please provide the file name!\n", stderr);
+      exit (1);
+    }
+  else
+    {
+      filename = argv[1];
     }
 
-    rdcmds(&cmd, filename);
-    go(cmd, arr);
-    putchar('\n');
+  rdcmds (&cmd, filename);
+  go (cmd, arr);
+  if (lastput != '\n')
+    putchar ('\n');
 
-#if !defined(RELEASE)
-    for (size_t i = 0; i < 4; ++i) {
-        for (size_t j = 16 * i; j < 16 * (i + 1); ++j)
-            printf("%3hhu, ", arr[j]);
-        putchar('\n');
+#ifndef RELEASE
+  for (size_t i = 0; i < 4; ++i)
+    {
+      for (size_t j = 16 * i; j < 16 * (i + 1); ++j)
+        printf ("%3hhu, ", arr[j]);
+      putchar ('\n');
     }
 #endif /* RELEASE */
 
-    return 0;
+  return 0;
 }
